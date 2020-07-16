b39f43874622ec3b77182356818c0b8650f410c5
Kojin <kojin@protonmail.com>
2020-07-16 13:36:20 -0400

    pcsx2-gui: some attempts to destroy the gs window

--- pcsx2/PluginManager.cpp
+++ pcsx2/PluginManager.cpp
@@ -884,11 +884,7 @@ void SysCorePlugins::ClosePlugin_GS()
 	if( GetMTGS().IsSelf() )
 		_generalclose( PluginId_GS );
 	else
-	{
-		if (!GSopen2)
-			PADclose();
 		GetMTGS().Suspend();
-	}
 }
 
 void SysCorePlugins::ClosePlugin_Mcd()
